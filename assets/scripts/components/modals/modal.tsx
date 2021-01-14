4ae0128134ae42b934590feed95a13e3dd9982dd
alejandro-antonio <alejandro@pcpartners.com.mx>
2021-01-14 12:58:39 -0600

    modal visibility transition

--- assets/scripts/components/modals/modal.tsx
+++ assets/scripts/components/modals/modal.tsx
@@ -22,37 +22,34 @@ export default class Modal extends React.Component<ModalPropsI, {}> {
 
     render = (): JSX.Element => {
         return (
-            <>
-                {this.props.show && (
-                    <div
-                        className="modal-component"
-                    >
-                        <div
-                            className="modal-component-content"
-                            style={{
-                                width: `${this.size}%`,
-                            }}
-                        >
-                            <div className="w-100 h-100 pt-2">
-                                <h5 className="text-center">
-                                    {this.props.title}
-                                <ButtonClose onClick={this.props.onClose} float="right" />
-                                </h5>
-                                {this.props.title && <hr/>}
-                                <div className="modal-component-body">
-                                    {(this.props.loading && (
-                                        <LoaderH position="center"/>
-                                    )) || (
-                                        <>
+            <div
+                className={"modal-component" + (this.props.show?" show":"") }
+            >
+                <div
+                    className="modal-component-content"
+                    style={{
+                        width: `${this.size}%`,
+                    }}
+                >
+                    <div className="w-100 h-100 pt-2">
+                        <h5 className="text-center">
+                            {this.props.title}
+                            <ButtonClose onClick={this.props.onClose} float="right" />
+                        </h5>
+                        {this.props.title && <hr />}
+                        <div className="modal-component-body">
+                            {(this.props.loading && (
+                                <LoaderH position="center" />
+                            )) || (
+                                    <>
                                         {this.props.children}
-                                        </>
-                                    )}
-                                </div>
-                            </div>
+                                    </>
+                                )}
                         </div>
                     </div>
-                )}
-            </>
+                </div>
+            </div>
+
         );
     };
 }
